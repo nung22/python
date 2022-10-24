4b4a9880881ec76cbe652c5d58499988f0c3d82e
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-23 22:52:19 -0700

    finished nested dictionaries & lists assignment

--- fundamentals/fundamentals/functions_basic_i.py
+++ fundamentals/fundamentals/functions_basic_i.py
@@ -1,13 +1,14 @@
 #1
 def number_of_food_groups():
     return 5
-# 
+# print 5
 print(number_of_food_groups())
 
 #3
 def number_of_books_on_hold():
     return 5
     return 10
+# print 5
 print(number_of_books_on_hold())
 
 
@@ -15,6 +16,7 @@ print(number_of_books_on_hold())
 def number_of_fingers():
     return 5
     print(10)
+# print 5
 print(number_of_fingers())
 
 
@@ -22,18 +24,21 @@ print(number_of_fingers())
 def number_of_great_lakes():
     print(5)
 x = number_of_great_lakes()
+# print 5, print None
 print(x)
 
 
 #6
 def add(b,c):
     print(b+c)
+# print 3, print 5, TypeError for attempting to add two values of type None
 print(add(1,2) + add(2,3))
 
 
 #7
 def concatenate(b,c):
     return str(b)+str(c)
+# print 25
 print(concatenate(2,5))
 
 
@@ -46,6 +51,7 @@ def number_of_oceans_or_fingers_or_continents():
     else:
         return 10
     return 7
+# print 100, print 10
 print(number_of_oceans_or_fingers_or_continents())
 
 
@@ -56,14 +62,18 @@ def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
     else:
         return 14
     return 3
+# print 7
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print 14
 print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print 21
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 
 
 #2
 def number_of_military_branches():
     return 5
+# TypeError for number_of_days_in_a_week_silicon_or_triangle_sides() since it is missing the 2 required arguments
 print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
 
 
@@ -71,41 +81,54 @@ print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_
 def addition(b,c):
     return b+c
     return 10
+# print 8
 print(addition(3,5))
 
 
 #11
 b = 500
+# print 500
 print(b)
 def foobar():
     b = 300
     print(b)
+# print 500
 print(b)
+# print 300
 foobar()
+# print 500
 print(b)
 
 
 #12
 b = 500
+# print 500
 print(b)
 def foobar():
     b = 300
     print(b)
     return b
+# print 500
 print(b)
+# print 300
 foobar()
+# print 500
 print(b)
 
 
 #13
 b = 500
+# print 500
 print(b)
 def foobar():
     b = 300
     print(b)
     return b
+# print 500
 print(b)
+# print 300
 b=foobar()
+# print 300
 print(b)
 
 
@@ -116,6 +139,7 @@ def foo():
     print(2)
 def bar():
     print(3)
+# print 1, print 3, print 2
 foo()
 
 
@@ -128,5 +152,7 @@ def foo():
 def bar():
     print(3)
     return 5
+# print 1, print 3, print 5
 y = foo()
+# print 10
 print(y)
---
+++ fundamentals/fundamentals/nested_dictionaries_&_lists.py
@@ -0,0 +1,96 @@
+# 1.Update Valies in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+print(x)
+print(students)
+print(sports_directory)
+print(z)
+
+# 2.Iterate Through a List of Dictionaries
+def iterateDictionary(some_list):
+    output_list = []
+    for some_dict in some_list:
+        entry = ""
+        for key,value in some_dict.items():
+            entry += key + ' - ' + value + ', '
+        entry = entry[:-2]
+        output_list.append(entry)
+    for element in output_list:
+        print(element)
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+"""
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+"""
+
+
+# 3.Get Values From a List of Dictionaries
+def iterateDictionary2(key_name, some_list):
+    for some_dict in some_list:
+        print(some_dict[key_name])
+
+iterateDictionary2('first_name',students)
+iterateDictionary2('last_name',students)
+
+
+# 4.Iterate Through a Dictionary with List Values
+def printInfo(some_dict):
+    for some_list in some_dict:
+        print(len(some_dict[some_list]), some_list.upper())
+        for element in some_dict[some_list]:
+            print(element)
+        print("")
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+printInfo(dojo)
+# output:
+"""
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon
+"""
