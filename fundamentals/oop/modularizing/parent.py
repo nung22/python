41f585ac3b433a731f303236e09f8e146ef82726
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-25 08:55:15 -0700

    finished modularizing assignment

--- fundamentals/oop/modularizing/parent.py
+++ fundamentals/oop/modularizing/parent.py
@@ -13,3 +13,9 @@ user = User("Anna")
 print(user.name)
 print(user.say_hello())
 
+print(__name__)
+
+if __name__ == "__main__":
+    print("the file is being executed directly")
+else:
+    print("The file is being executed because it is imported by another file. The file is called: ", __name__)
