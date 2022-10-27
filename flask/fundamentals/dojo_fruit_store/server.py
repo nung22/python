37c413231034d7980aa0c152782f9a8b83c22d7d
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-27 12:51:17 -0700

    finished dojo fruit store assignment

--- flask/fundamentals/dojo_fruit_store/server.py
+++ flask/fundamentals/dojo_fruit_store/server.py
@@ -1,3 +1,4 @@
+from datetime import datetime,time
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
@@ -8,8 +9,15 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
-
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    numStrawberries = int(request.form.get("strawberry"))
+    numRaspberries = int(request.form.get("raspberry"))
+    numApples = int(request.form.get("apple"))
+    print(f"Charging {first_name} {last_name} for {numStrawberries+numRaspberries+numApples} fruits")
+    return render_template("checkout.html",num_strawberry=numStrawberries,num_raspberry=numRaspberries,num_apple=numApples,fname=first_name,lname=last_name,studentID = request.form.get("student_id"),dateAndTime = datetime.now().strftime("%m/%d/%y")
+    )
+# March 23rd 2019 5:15:35 PM
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
