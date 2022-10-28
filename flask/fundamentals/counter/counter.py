e02083e6992cd33f702f98b58d29ee0443409b5f
Nicholas Ung <ung.nicholask@gmail.com>
2022-10-27 23:34:40 -0700

    updated counter assignment

--- flask/fundamentals/counter/counter.py
+++ flask/fundamentals/counter/counter.py
@@ -3,21 +3,35 @@ app = Flask(__name__)
 app.secret_key = 'you shall not pass!'
 
 @app.route('/')
-def counter():
-  if 'views' in session:
-    session['views'] = session.get('views') + 1 # reads and updates number of page views
+def counter(increment=1):
+  if 'visits' in session:
+    session['visits'] = session.get('visits') + increment # reads and updates number of page visits
   else:
-    session['views'] = 1 # sets initial view for session
-  return render_template("index.html", views=session['views'])
+    session['visits'] = 1 # sets initial view for session
+  if 'actualVisits' in session:
+    session['actualVisits'] = session.get('actualVisits') + 1 # reads and updates number of page actual visits
+  else:
+    session['actualVisits'] = 1 # sets initial view for session
+  return render_template("index.html", visits=session['visits'], actual_visits=session['actualVisits'])
+
+@app.route('/adjust_increment', methods=['POST'])
+def adjust_increment():
+  print(request.form.get('increment'))
+  increment = request.form.get('increment')
+  print(increment)
+  return redirect('/')
+
 
 @app.route('/destroy_session')
 def destroy_session():
-  session.pop('views')
+  session.pop('visits')
+  session.pop('actualVisits')
   return redirect('/')
 
 @app.route('/plus_two')
 def plus_two():
-  session['views'] = session.get('views') + 1
+  session['visits'] = session.get('visits') + 1
+  session['actualVisits'] = session.get('actualVisits') - 1
   return redirect('/')
 
 if __name__=='__main__':
