b13df47ce4c61d8770da3dd0e3506ba8d896ac67
nung22 <ung.nicholask@gmail.com>
2022-11-03 20:30:46 -0700

    Add Class Method To Return All Book Objects

--- flask_mysql/crud/books/flask_app/models/book.py
+++ flask_mysql/crud/books/flask_app/models/book.py
@@ -13,6 +13,18 @@ class Book:
         self.updated_at = db_data['updated_at']
     
     @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM books;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL(DATABASE).query_db(query)
+        # Create an empty list to append our instances of books
+        books = []
+        # Iterate over the db results and create instances of books with cls.
+        for book in results:
+            books.append( cls(book) )
+        return books
+    
+    @classmethod
     def save(cls, data):
         query = "INSERT INTO books ( title, num_of_pages, created_at , updated_at ) VALUES (%(title)s,%(num_of_pages)s,NOW(),NOW());"
         return connectToMySQL(DATABASE).query_db(query, data)
@@ -26,11 +38,11 @@ class Book:
         book = cls( results[0] )
         for db_row in results:
             # Now we parse the author data to make instances of authors and add them into our list.
-          author_data = {
-                  "id" : db_row["authors.id"],
-                  "name" : db_row["name"],
-                  "created_at" : db_row["authors.created_at"],
-                  "updated_at" : db_row["authors.updated_at"]
-          }
-          book.favorited_by_author.append( author.Author( author_data ) )
+            author_data = {
+                    "id" : db_row["authors.id"],
+                    "name" : db_row["name"],
+                    "created_at" : db_row["authors.created_at"],
+                    "updated_at" : db_row["authors.updated_at"]
+            }
+            book.favorited_by_author.append( author.Author( author_data ) )
         return book
